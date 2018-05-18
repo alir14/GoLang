ce23ce35e16f3fc4cdfdf36c39ebad1217a58476
Rezazadeh Khorakpaz <mohammadali.rezazadehkhorakpaz@monster.com>
2018-05-18 18:54:06 +0800

    adding projetc - looping in data

---
+++ project.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type SitemapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+
+func main() {
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	// string_body := string(bytes)
+	// fmt.Println(string_body)
+	resp.Body.Close()
+
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+
+	//fmt.Println(s.Locations)
+
+	for index, Item := range s.Locations {
+		fmt.Printf("\n %d %s", index, Item)
+	}
+
+}
