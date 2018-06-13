d70723e1dca363258b395c854d3805b09f458620
Rezazadeh Khorakpaz <mohammadali.rezazadehkhorakpaz@monster.com>
2018-06-13 13:54:06 +0800

    go panic recover

--- concurrency.go
+++ concurrency.go
@@ -8,13 +8,29 @@ import (
 
 var waitgroup sync.WaitGroup
 
+func cleanup() {
+
+	// r:=recover()
+	// if r!= nil{
+	// 	// do something
+	// }
+	defer waitgroup.Done()
+
+	if r := recover(); r != nil {
+		fmt.Println("recovered in cleanup", r)
+	}
+}
+
 func say(s string) {
+	defer cleanup()
+
 	for i := 0; i < 3; i++ {
 		fmt.Println(s)
 		time.Sleep(time.Millisecond * 100)
+		if i == 2 {
+			panic("oh dear case 2")
+		}
 	}
-
-	waitgroup.Done()
 }
 
 func main() {
@@ -22,9 +38,10 @@ func main() {
 	go say("hey")
 	waitgroup.Add(1)
 	go say("There")
-	// waitgroup.Wait()
 
-	waitgroup.Add(1)
-	go say("hi")
 	waitgroup.Wait()
+
+	// waitgroup.Add(1)
+	// go say("hi")
+	// waitgroup.Wait()
 }
